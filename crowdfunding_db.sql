3834d389bdf1fe6f29c3f34e21f588b4b5daed8e
yvochu <yvonnechukwu@yahoo.com>
2024-09-25 21:06:42 -0500

    Add files via upload

---
+++ crowdfunding_db.sql
@@ -0,0 +1,42 @@
+--Campaign Table Schema
+CREATE TABLE campaign (
+    cf_id INT PRIMARY KEY,
+    contact_id INT,
+    company_name VARCHAR(255),
+    description TEXT,
+    goal DECIMAL(10, 2),
+    pledged DECIMAL(10, 2),
+    outcome VARCHAR(50),
+    backers_count INT,
+    country VARCHAR(50),
+    currency VARCHAR(10),
+    launch_date TIMESTAMP,
+    end_date TIMESTAMP,
+    category_id VARCHAR(50),
+    subcategory_id VARCHAR(50),
+    FOREIGN KEY (contact_id) REFERENCES contact(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+-- Category Table Schema
+CREATE TABLE category (
+    category_id VARCHAR(50) PRIMARY KEY,
+    category VARCHAR(255)
+);
+-- Subcategory Table Schema
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(50) PRIMARY KEY,
+    subcategory VARCHAR(255),
+    category_id VARCHAR(50),
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+-- Contact Table Schema:
+CREATE TABLE contact (
+    contact_id INT PRIMARY KEY
+    
+);
+
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contact;
+SELECT * FROM campaign;
